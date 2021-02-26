9a4673d18a6ce1fd5ec24a798d255cb84462cfea
sudharshansprabhu101997 <79701166+sudharshansprabhu101997@users.noreply.github.com>
2021-02-26 18:54:09 +0530

    Update csvFormater.js

--- src/components/csvFormater.js
+++ src/components/csvFormater.js
@@ -6,20 +6,106 @@ const CsvFormater = () => {
   const csv_array = [];
   csv_array.push(['SR. NO', 'ID NUMBER', 'EMP NAME', 'IN TIME', 'OUT TIME', 'TOTAL DURATION', 'PUNCH RECORDS', 'WORK DURATION', 'PRESENT/ABSENT', 'STATUS', 'REMARK', 'TOTAL DURATION 1']);
   let regExp = /\(([^)]+)\)/;
+  let row = [];
   const handlerCsvRow = (data) => {
-    let row = [];
 
-    let punchRecordLits = data[data.length - 1].split(',');
 
-    for (let i = 0; i < punchRecordLits.length - 1; i++)
+    let punchRecordList = data[data.length - 1].split(',');
+    let valid = true;
+
+    // let punchObject = dataParser(punchRecordList);
+    // console.log(punchObject);
+    const cols = [0, 1, 2, 9, 10, 13, 18, 11, 16,]
+    for (let i = 0; i < data.length - 1; i++)
     {
-      let punchRecord = punchRecordLits[i].split(':');
-      punchRecord[2] = punchRecord[2].replace(/\(TD\)/, "(Downstair)");
-      console.log(punchRecord[2]);
+      if (cols.indexOf(i))
+        row.push(data[i]);
     }
+    // for (let i = 0; i < punchRecordLits.length - 1; i++)
+    // {
+    //   let punchRecord1 = punchRecordLits[i].split(':');
+    //   let punchRecord2 = punchRecordLits[i + 1].split(':');
+    //   let stair1 = /\(([^)]+)\)/.exec(punchRecord1[2])[1];
+    //   let stair2 = /\(([^)]+)\)/.exec(punchRecord1[2])[1];
+    //   let punch1 = punchRecord1[2].substr(0, 1);
+    //   let punch2 = punchRecord2[2].substr(0, 1);
+    //   if (punch1 === 'i' && punch2 === 'o')
+    //   {
+    //     valid = true;
+    //   }
+    //   //   let punch = args.substr(0,1);
+
+    //   // ValidPunch(punchRecord[2]);
+
+
+    //   // punchRecord[2] = punchRecord[2].replace(/\(TD\)/, "(Downstair)");
+
+
+    // }
 
 
   };
+
+
+  const dataParser = (data) => {
+    let punchRecordPrev = [];
+    let punchRecordCur = [];
+    let punchRecordNext = [];
+    let punchObject = [];
+
+    for (let i = 0; i < data.length; i++)
+    {
+
+      if (i === 0)
+      {
+        punchRecordCur = data[i].split(':')[2];
+        punchRecordNext = data[i + 1].split(':')[2];
+        if (!punchRecordCur.includes('in'))
+        {
+          punchRecordCur = 'in' + punchRecordCur;
+        }
+      }
+      else if (i !== data.length - 1)
+      {
+        punchRecordPrev = data[i].split(':')[2];
+        punchRecordCur = data[i].split(':')[2];
+        punchRecordNext = data[i + 1].split(':')[2];
+
+        if (!(punchRecordCur.includes('in') && punchRecordCur.includes('out')))
+        {
+          if (punchRecordPrev.includes('in'))
+          {
+            punchRecordCur = 'in' + punchRecordCur;
+          }
+          else if (punchRecordPrev.includes('out'))
+          {
+            punchRecordCur = 'out' + punchRecordCur;
+          }
+        }
+      }
+      else
+      {
+        if (!punchRecordCur.includes('out'))
+        {
+          punchRecordCur = 'out' + punchRecordCur;
+        }
+
+      }
+
+      punchObject.push(punchRecordCur);
+    }
+    return punchRecordCur;
+  };
+
+
+  // const ValidPunch = (args) => {
+  //   let valid = true;
+  //   let stair = /\(([^)]+)\)/.exec(args)[1];
+  //   let punch = args.substr(0,1);
+
+  //   if(punch === 'i' && )
+
+  // };
   const handleOnDrop = (data) => {
 
     console.log(data);
@@ -53,7 +139,6 @@ const CsvFormater = () => {
       >
         <span>Drop CSV file here or click to upload.</span>
       </CSVReader>
-
     </div>
   )
 }
